bcf72cbc777bdc29535120cb0d47dfa59682cc43
YubaC <2383931696@qq.com>
2023-06-23 10:26:07 +0800

    Use export

--- js/common.js
+++ js/common.js
@@ -2,29 +2,13 @@
 $("#header").load("header.html");
 $("#footer").load("footer.html");
 
-// 1. 查找localStorage中是否有token
-// 2. 如果没有，查找cookie中是否有token
-// 3. 如果没有，跳转到登录页面
-// 4. 如果有，加载数据
+// 检查token是否存在
+var token = localStorage.getItem("token") || getCookie("token");
 
-// 1. 查找localStorage中是否有token
-var token = localStorage.getItem("token");
-if (token == null) {
-    // 2. 如果没有，查找cookie中是否有token
-    token = getCookie("token");
-    if (token == null || token == "") {
-        // 3. 如果没有，跳转到登录页面
-        window.location.href = `login.html?redirct=${b64EncodeUnicode(
-            window.location.href
-        )}`;
-    } else {
-        // 刷新cookie的过期时间
-        setCookie("token", token, 7);
-        // 4. 如果有，加载数据
-        loadData();
-    }
+if (!token) {
+    window.location.href = `login.html?redirct=${b64EncodeUnicode(window.location.href)}`;
 } else {
-    // 4. 如果有，加载数据
+    setCookie("token", token, 7);
     loadData();
 }
 
@@ -35,34 +19,4 @@ function logout() {
     delCookie("token");
     // 3. 跳转到登录页面
     window.location.href = "login.html";
-}
-
-// cookie操作
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-    var expires = "expires=" + d.toGMTString();
-    document.cookie =
-        cname + "=" + cvalue + "; " + expires + "; SameSite=None; Secure";
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(";");
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i].trim();
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-function delCookie(name) {
-    var exp = new Date();
-    exp.setTime(exp.getTime() - 1);
-    var cval = getCookie(name);
-    if (cval != null) {
-        setCookie(name, cval, -1);
-    }
 }
--- js/functions.js
+++ js/functions.js
@@ -5,6 +5,7 @@ const db = {
     base: "data/",
 };
 
+// !URL处理
 function getUrlParam(para) {
     var search = location.search; //页面URL的查询部分字符串
     var arrPara = new Array(); //参数数组。数组单项为包含参数名和参数值的字符串，如“para=value”
@@ -34,6 +35,40 @@ function getUrlParam(para) {
     }
 }
 
+/**
+ * 返回用于请求的url，去除url中的../和./
+ * @param {string} origionUrl The original url
+ */
+function getRequestUrl(origionUrl) {
+    // 处理 url
+    let url = origionUrl
+        .replace(/\/{2,}/g, "/")
+        .replace(/\/\.\//g, "/")
+        .replace(/\/\.$/, "");
+
+    // 将 url 分割成路径名和文件名两部分
+    let lastSlashIndex = url.lastIndexOf("/");
+    let path = url.substring(0, lastSlashIndex + 1);
+    let filename = url.substring(lastSlashIndex + 1);
+
+    // 处理路径名，去掉 ../ 和 ./
+    let pathArr = path.split("/");
+    let resultPathArr = [];
+    for (let i = 0; i < pathArr.length; i++) {
+        if (pathArr[i] === "..") {
+            resultPathArr.pop();
+        } else if (pathArr[i] !== "." && pathArr[i] !== "") {
+            resultPathArr.push(pathArr[i]);
+        }
+    }
+    let resultPath = resultPathArr.join("/");
+
+    // 返回处理后的 url
+    return resultPath + "/" + filename;
+}
+
+
+// !Base64加密解密
 // base64加密的函数
 function b64EncodeUnicode(str) {
     if (str == null) return null;
@@ -59,3 +94,73 @@ function b64DecodeUnicode(str) {
             .join("")
     );
 }
+
+
+// !cookie操作
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    var expires = "expires=" + d.toGMTString();
+    document.cookie =
+        cname + "=" + cvalue + "; " + expires + "; SameSite=None; Secure";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function delCookie(name) {
+    var exp = new Date();
+    exp.setTime(exp.getTime() - 1);
+    var cval = getCookie(name);
+    if (cval != null) {
+        setCookie(name, cval, -1);
+    }
+}
+
+// !Github API
+// 获取 Github 上的文件内容
+async function getFileContent(path) {
+    console.log(getRequestUrl(db.base + url + "/" + path));
+    const res = await octokit.request(
+        `GET /repos/{owner}/{repo}/contents/{path}?t=${new Date().getTime()}`,
+        {
+            owner: db.owner,
+            repo: db.repo,
+            path: getRequestUrl(db.base + url + "/" + path),
+        }
+    );
+    const { download_url } = res.data;
+    return { download_url };
+}
+
+// !其他
+function showErrorMsg(element, msg1, msg2) {
+    $(element).after(
+        `<div class="alert alert-danger">${msg1}<br><h6><b>${msg2}</b></h6></div>`
+    );
+}
+
+function addProgressBar(element) {
+    const progressBar = $(`<div class="progress">
+                              <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar"></div>
+                          </div>`)
+        .insertBefore(element)
+        .find(".progress-bar");
+    const updateProgressBar = (percentage) => {
+        progressBar.css("width", `${percentage}%`);
+    };
+    return { progressBar, updateProgressBar };
+}
+
+
+
+export { db, getUrlParam, getRequestUrl, b64EncodeUnicode, b64DecodeUnicode, setCookie, getCookie, delCookie, getFileContent, showErrorMsg, addProgressBar };
